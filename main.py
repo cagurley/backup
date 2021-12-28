de801ee1bc42b6505397b8cea340852f5d5b91f5
Atticus Gurley <atticus.gurley@gmail.com>
2021-12-27 21:17:57 -0500

    Fixed exception handling bug

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
         for p in backup.CONFIG['src']:
             backup.backup(p, backup.CONFIG['dst'].joinpath(p.name))
     except Exception as e:
-        backup.plog(e)
+        backup.plog(repr(e))
     finally:
         backup.close_log()
